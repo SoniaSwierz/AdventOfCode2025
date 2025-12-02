de80025faf5af21eb96b0a8e4026f6563f7759bb
Sonia Świerz <sonia.swierz@gmail.com>
2025-12-02 15:39:47 +0100

    Day 1 and 2 Solution

---
+++ AoC2025/AoC2025/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Day1 { 
+
+    public class Program {
+
+        public static void Main() {
+            string filePath = @"C:\Users\sonia\Documents\GitHub\AdventOfCode2025\AoC2025\AoC2025\input.txt";
+
+            if (!File.Exists(filePath))
+                return;
+
+            int currentNumber = 50;
+            int finalResult = 0;
+
+            string[] lines = File.ReadAllLines(filePath);
+            int bonusTimes = 0;
+            int bonusSum = 0;
+
+            foreach (string line in lines) {
+                int modifyValue = int.Parse(line.Substring(1));
+                bonusTimes = 0;
+
+                if (line[0] == 'R') {
+                    currentNumber += modifyValue;
+
+                    bonusTimes += currentNumber / 100;
+                        
+                    currentNumber %= 100;
+
+                    if (currentNumber == 0) {
+                        finalResult++;
+                        bonusTimes--;
+                    }
+
+                } else {
+                    if (currentNumber == 0)
+                        bonusTimes--;
+
+                    bonusTimes += (modifyValue / 100);
+                    modifyValue %= 100;
+
+                    currentNumber -= modifyValue;
+    
+                    if (currentNumber == 0)
+                        finalResult++;
+
+                    if(currentNumber < 0) {
+                        currentNumber += 100;
+                        bonusTimes++;
+                    }
+
+                }
+                bonusSum += bonusTimes;
+            }
+            Console.WriteLine(finalResult);
+            Console.WriteLine(finalResult + bonusSum);
+        }
+    }
+}
---
+++ AoC2025/Day2/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Day2 {
+
+    internal class Program {
+
+        public static HashSet<ulong> uniqueNumbers = new HashSet<ulong>();
+
+        public static string MakeString(long number, int times) {
+            string result = "";
+            for(int i = 0; i < times; i++)
+                result += number;
+
+            return result;
+        }
+
+        public static void CountInvalidIDsInRange(string leftString, string rightString, int times) {
+
+            long leftRange = long.Parse(leftString);
+            long rightRange = long.Parse(rightString);
+
+            int left = int.Parse(leftString.Substring(0, (int)Math.Ceiling((double)(leftString.Length) / times)));
+            int right = int.Parse(rightString.Substring(0, (int)Math.Ceiling((double)(rightString.Length) / times)));
+
+            if (times % 2 == 1 && left > right)
+                (left, right) = (right, left);
+            else if (left > right)
+                left /= 10;
+
+            string numberString;
+            long number;
+            
+            for (int i = left; i <= right; i++) {
+                numberString = MakeString(i, times);
+                number = long.Parse(numberString);
+                
+                if (number <= rightRange && number >= leftRange) {
+                    uniqueNumbers.Add((ulong)number);
+                    //Console.WriteLine(number + " " + leftRange + " " + rightRange);
+                }
+            }
+        }
+
+        public static void Solve() {
+            uniqueNumbers.Clear();
+            long resultPart1 = 0;
+            long resultPart2 = 0;
+
+            string file = @"C:\Users\sonia\Documents\GitHub\AdventOfCode2025\AoC2025\Day2\input.txt";
+
+            using (StreamReader streamReader = new StreamReader(file)) {
+                string line = streamReader.ReadLine();
+
+                if (line == null)
+                    return;
+
+                string[] ranges = line.Split(',');
+                foreach (string range in ranges) {
+                    uniqueNumbers.Clear();
+
+                    string[] parts = range.Split('-');
+
+                    int size = parts[1].Length;
+
+                    // PART 1 SOLUTION -----------------------
+                    CountInvalidIDsInRange(parts[0], parts[1], 2);
+                    foreach (long number in uniqueNumbers) {
+                        resultPart1 += number;
+                    }
+
+                    // PART 2 SOLUTION -----------------------
+                    for (int i = 3; i <= size; i++) {
+                        CountInvalidIDsInRange(parts[0], parts[1], i);
+                    }
+
+                    foreach (long number in uniqueNumbers) {
+                        resultPart2 += number;
+                    }
+                }
+                Console.WriteLine($"Part 1: {resultPart1}");
+                Console.WriteLine($"Part 2: {resultPart2}");
+            }
+        }
+
+        public static void Main() {
+            Solve();
+        }
+    }
+}
